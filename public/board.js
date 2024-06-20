7683c54086daf2cdc0b3f425fa1c0f1a756fda9f
Lev <lev.bereza@gmail.com>
2024-06-20 17:53:44 +0300

    Added pieces on the board

--- public/board.js
+++ public/board.js
@@ -1,8 +1,12 @@
 const board = document.querySelector('#board')
+/*
+const intialBlackState = document.querySelector('.initial__black__state');
+const intialWhiteState = document.querySelector('.initial__white__state');
+*/
 
 const quarters = {
-    firstQuarter: document.querySelector('#first__quarter'),
     secondQuarter: document.querySelector('#second__quarter'),
+    firstQuarter: document.querySelector('#first__quarter'),
     thirdQuarter: document.querySelector('#third__quarter'),
     fourthQuarter: document.querySelector('#fourth__quarter'),
 };
@@ -10,10 +14,13 @@ const quarters = {
 const drawTriangles = () => {
     let top = 0;
     let counter = 0;
+    let id = 0;
     for (const quarter of Object.values(quarters)) {
         for (let i = 0; i < 6; i++) {
             const triangle = document.createElement('div');
             triangle.classList.add('triangle');
+            triangle.setAttribute('triangle-id', id.toString());
+            id++;
 
             if (top < 2 && counter % 2 === 0) {
                 triangle.classList.add('top__light__triangle');
@@ -33,4 +40,28 @@ const drawTriangles = () => {
     }
 }
 
+const setPieces = () => {
+    const initialBlackState = document.createElement('div');
+    const initialWhiteState = document.createElement('div');
+
+    const yourStartTriangle = document.querySelector('[triangle-id="11"]');
+    console.log(yourStartTriangle);
+    const opponentStartTriangle = document.querySelector('[triangle-id="12"]');
+
+    for (let i = 0; i < 15; i++) {
+        const newBlackPiece = document.createElement('div');
+        const newWhitePiece = document.createElement('div');
+
+        newBlackPiece.classList.add('initial__black__state');
+        newWhitePiece.classList.add('initial__white__state');
+
+        initialBlackState.append(newBlackPiece);
+        initialWhiteState.append(newWhitePiece);
+    }
+
+    yourStartTriangle.append(initialBlackState);
+    opponentStartTriangle.append(initialWhiteState);
+}
+
 drawTriangles();
+setPieces();
