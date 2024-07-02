7965900ea097e627139e7d5fd66f6c67c988f9a1
Lev <lev.bereza@gmail.com>
2024-07-02 12:36:06 +0300

    Completely flipping the board for the opponent

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@
 
 import { socket, stylePieces } from "../public/socket.js";
 
+const TRIANGLES_NUMBER = 23;
 const triangles = document.querySelectorAll('.triangle');
 let draggedElement;
+
+/*const opponentTriangles = {
+    0:
+}*/
 
 const dragStart = (e) => {
     draggedElement = e.target;
@@ -21,9 +26,12 @@ const dragDrop = (e) => {
     let children = e.target.childNodes;
     stylePieces(e, draggedElement, isTopTriangle, children);
 
+    const triangleId = Number(e.target.getAttribute('triangle-id'));
+    const opponentTriangle = (TRIANGLES_NUMBER - triangleId).toString();
+
     const data = {
         pieceId: draggedElement.id,
-        triangleId: e.target.getAttribute('triangle-id')
+        triangleId: opponentTriangle
     };
 
     socket.send(JSON.stringify(data));
